902f7226076f214f10581698f4440ac1984afc61
Kevin Balabaud <kevinblbd@icloud.com>
2023-04-25 16:31:40 +0200

    modify style.js

--- styles.js
+++ styles.js
@@ -10,39 +10,4 @@ export default createGlobalStyle`
   body {
     margin: 0;
     font-family: system-ui;
-  }
-
-  /* __________________ not necessary */
-
-  .card {
-  width: 70%;
-  padding: 20px;
-  margin-inline: auto;
-  margin-block: 20px;
-  background-color: var(--color-water);
-}
-
-.card--green {
-  background-color: lightgreen;
-}
-
-.card--red {
-  background-color: lightcoral;
-}
-
-.card--yellow {
-  background-color: lightyellow;
-}
-.card--orange {
-  background-color: orange;
-}
-
-.card--broun {
-  background-color: burlywood;
-}
-.app > li {
-  list-style: none;
-}
-
-
-`;
+  }`;
